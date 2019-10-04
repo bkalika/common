6496f88e1cb5835ed5184fc4152982c5976a4d22
bkalika <bogdan.kalika@gmail.com>
2019-10-04 22:39:48 +0300

    have done HW-3

--- python_functionality/homework.py
+++ python_functionality/homework.py
@@ -14,7 +14,7 @@ def task_1_fix_names_start_letter(data: DT) -> DT:
         fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
-    return [{k: (v.capitalize() if isinstance(v, str) else v) for (k, v) in d.items()} for d in data]
+    return ({i: (j.capitalize() if isinstance(j, str) else j) for (i, j) in d.items()} for d in data)
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
@@ -25,7 +25,11 @@ def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
        remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
     """
-    pass
+    for item_in_dict in data:
+        for key_in_list in redundant_keys:
+            if key_in_list in item_in_dict.keys():
+                del item_in_dict[key_in_list]
+    return data
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
@@ -35,21 +39,24 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
         find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
         >>> [{'name': 'Alex', 'age': 26}]
     """
-    pass
+    for item_in_dict in data:
+        if value in item_in_dict.values():
+            return [item_in_dict]
 
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     Find and return minimum value from list
     """
-    pass
+    return min(data) if len(data) > 0 else None
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    pass
+    new_list = [str(i).lower() for i in data]
+    return min(new_list) if len(data) > 0 else None
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
@@ -58,21 +65,27 @@ def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     Returns:
 
     """
-    pass
+    new_list = []
+    for item_in_data in data:
+        new_list.append(item_in_data.get(key))
+    minimum = min(i for i in new_list if i is not None)
+    for item in data:
+        if minimum == item.get(key):
+            return item
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    pass
+    return max(max(list(data)))
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
     Find sum of all items in given list
     """
-    pass
+    return sum(i for i in data)
 
 
 def task_9_sum_characters_positions(text: str) -> int:
@@ -88,7 +101,7 @@ def task_9_sum_characters_positions(text: str) -> int:
         >>> 532
 
     """
-    pass
+    return sum(ord(i) for i in text)
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
@@ -102,4 +115,6 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 3
     """
-    pass
+    for iterable in range(2, 201):
+        if all(iterable % i != 0 for i in range(2, iterable)):
+            yield iterable
