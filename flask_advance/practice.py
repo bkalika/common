9a7597c51fa12f23faeebd6a1b5ecc71ecb9a9b8
bkalika <bogdan.kalika@gmail.com>
2019-11-02 17:12:43 +0200

    lecture flask_advance

--- flask_advance/practice.py
+++ flask_advance/practice.py
@@ -1,8 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, url_for, session
+from werkzeug.utils import secure_filename, redirect
+from blueprint.main import vegetable
+import os
 
 app = Flask(__name__)
 
 app_list = []
+
+app.config["SECRET_KEY"] = "secret_key_kalika"
+app.register_blueprint(vegetable)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def return_info():
@@ -12,7 +19,7 @@ def return_info():
         app_list.append("1")
         print(app_list)
         return 'OK'
-    return 'ok'
+    return 'OK'
 
 
 @app.route('/request')
@@ -24,16 +31,43 @@ def request_object_example():
         "request.data": request.data
     }
     print(data)
-    return 'ok'
+    return "ok"
+
 
 @app.route('/save_file', methods=['POST'])
 def save_file():
-    file = request.files
-    return
+    file = request.files['file']
+    path = os.path.join('file', secure_filename(file.filename))
+    file.save(path)
+    return "ok"
+
+
+@app.route('/cookies')
+def get_cookies():
+    cookies = request.cookies
+    return jsonify(cookies)
+
 
 @app.route('/home')
 def get_home():
+    session["login"] = 'Ok'
     return render_template('main.html')
+    # return redirect(url_for('return_info'))
+
+
+@app.route('/test_session')
+def get_session():
+    print(session.get('login'))
+    app.logger.info("INFO test")
+    app.logger.debug("A value for debugging")
+    app.logger.warning("A warning occurred (%d apples)", 42)
+    app.logger.error("An error occurred")
+    return 'Ok session'
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return render_template("error_404.html")
 
 
 if __name__ == '__main__':
--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -10,42 +10,43 @@ def get_home_page():
     return render_template("home.html", data=get_data())
 
 
-@app.route("/iwatch", methods=['GET'])
-def get_page_iwatch():
+@app.route("/<value>", methods=['GET'])
+def get_page_iwatch(value):
+    print(value)
     return render_template("iwatch.html", data=get_data()[0])
 
 
-@app.route("/headphones", methods=['GET'])
-def get_page_headphones():
-    return render_template("headphones.html", data=get_data()[1])
-
-
-@app.route("/IPod", methods=['GET'])
-def get_page_ipod():
-    return render_template("IPod.html", data=get_data()[2])
-
-
-@app.route("/calculator", methods=['GET'])
-def get_page_calculator():
-    return render_template("calculator.html", data=get_data()[3])
-
-
-@app.route("/coffeemaker", methods=['GET'])
-def get_page_coffeemaker():
-    return render_template("coffeemaker.html", data=get_data()[4])
-
-
-@app.route("/battery_charger", methods=['GET'])
-def get_page_battery_charger():
-    return render_template("battery_charger.html", data=get_data()[5])
-
-
-@app.route("/author", methods=['GET'])
-def get_page_author():
-    name = "Author:"
-    about = "I did it with little looking"
-    surname = "Bohdan Kalika"
-    return render_template("author.html", name=name, about=about, surname=surname)
+# @app.route("/headphones", methods=['GET'])
+# def get_page_headphones():
+#     return render_template("headphones.html", data=get_data()[1])
+#
+#
+# @app.route("/IPod", methods=['GET'])
+# def get_page_ipod():
+#     return render_template("IPod.html", data=get_data()[2])
+#
+#
+# @app.route("/calculator", methods=['GET'])
+# def get_page_calculator():
+#     return render_template("calculator.html", data=get_data()[3])
+#
+#
+# @app.route("/coffeemaker", methods=['GET'])
+# def get_page_coffeemaker():
+#     return render_template("coffeemaker.html", data=get_data()[4])
+#
+#
+# @app.route("/battery_charger", methods=['GET'])
+# def get_page_battery_charger():
+#     return render_template("battery_charger.html", data=get_data()[5])
+#
+#
+# @app.route("/author", methods=['GET'])
+# def get_page_author():
+#     name = "Author:"
+#     about = "I did it with little looking"
+#     surname = "Bohdan Kalika"
+#     return render_template("author.html", name=name, about=about, surname=surname)
 
 
 if __name__ == "__main__":
